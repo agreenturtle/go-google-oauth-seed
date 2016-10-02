12ccee09d6519e27bda674650dc39f89fa13bef7
James Chen <james.lai.chen@gmail.com>
2016-10-01 22:15:54 -0700

    alignment changes

--- server.go
+++ server.go
@@ -2,68 +2,68 @@ package main
 
 import (
 	"fmt"
-  "log"
+	"log"
 	"net/http"
-  "os"
+	"os"
 
-  // Third Party Libraries
+	// Third Party Libraries
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
-  "github.com/julienschmidt/httprouter"
+	"github.com/julienschmidt/httprouter"
 )
 
 // Your credentials should be obtained from the Google
 // Developer Console (https://console.developers.google.com).
 var oauthCfg = &oauth2.Config{
-  ClientID:     os.Getenv("GO_GOOGLE_OAUTH_SEED_CLIENT_ID"),
-  ClientSecret: os.Getenv("GO_GOOGLE_OAUTH_SEED_CLIENT_SECRET"),
-  RedirectURL:  "http://localhost:8000/auth/google/callback",
-  Scopes: []string{"profile"},
-  Endpoint: google.Endpoint,
+	ClientID:     os.Getenv("GO_GOOGLE_OAUTH_SEED_CLIENT_ID"),
+	ClientSecret: os.Getenv("GO_GOOGLE_OAUTH_SEED_CLIENT_SECRET"),
+	RedirectURL:  "http://localhost:8000/auth/google/callback",
+	Scopes: []string{"profile"},
+	Endpoint: google.Endpoint,
 }
 
 func GoogleHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	// Redirect user to Google's consent page to ask for permission
 	// for the scopes specified above.
-  http.Redirect(w,r, oauthCfg.AuthCodeURL("state"), http.StatusTemporaryRedirect)
+	http.Redirect(w,r, oauthCfg.AuthCodeURL("state"), http.StatusTemporaryRedirect)
 }
 
 func GoogleCallbackHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-  // Uses temporary token provided by Google to get Token. Temporary token is
-  // found in the "code" attribute in the request.
-  code := r.FormValue("code")
-  token, err := oauthCfg.Exchange(oauth2.NoContext, code)
-  // if error exists,
-  if err != nil {
-    http.Redirect(w, r, "/error", http.StatusTemporaryRedirect)
-  } else {
-    fmt.Printf("Logging Token to prevent error %v\n", token)
-    http.Redirect(w, r, "/index", http.StatusTemporaryRedirect)
-  }
+	// Uses temporary token provided by Google to get Token. Temporary token is
+	// found in the "code" attribute in the request.
+	code := r.FormValue("code")
+	token, err := oauthCfg.Exchange(oauth2.NoContext, code)
+	// if error exists,
+	if err != nil {
+		http.Redirect(w, r, "/error", http.StatusTemporaryRedirect)
+	} else {
+	fmt.Printf("Logging Token to prevent error %v\n", token)
+		http.Redirect(w, r, "/index", http.StatusTemporaryRedirect)
+	}
 }
 
 func loginPageHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-  fmt.Println("LOAD LOGIN PAGE")
-  http.ServeFile(w, r, "src/login/index.html")
+	fmt.Println("LOAD LOGIN PAGE")
+	http.ServeFile(w, r, "src/login/index.html")
 }
 func indexPageHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-  http.ServeFile(w, r, "src/index/index.html")
+	http.ServeFile(w, r, "src/index/index.html")
 }
 func errorPageHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-  http.ServeFile(w, r, "src/error/index.html")
+	http.ServeFile(w, r, "src/error/index.html")
 }
 
 func main() {
-  router := httprouter.New()
+	router := httprouter.New()
 
-  // OAuth
-  router.GET("/auth/google/", GoogleHandler)
-  router.GET("/auth/google/callback", GoogleCallbackHandler)
+	// OAuth
+	router.GET("/auth/google/", GoogleHandler)
+	router.GET("/auth/google/callback", GoogleCallbackHandler)
 
-  // Routes
-  router.GET("/", loginPageHandler)
-  router.GET("/index/", indexPageHandler)
-  router.GET("/error/", errorPageHandler)
+	// Routes
+	router.GET("/", loginPageHandler)
+	router.GET("/index/", indexPageHandler)
+	router.GET("/error/", errorPageHandler)
 
-  log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
